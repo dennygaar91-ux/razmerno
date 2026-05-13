5571d94c5ece5db9c8cdecf1e4bbf1e6efe3b427
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-13 08:05:18 +0300

    import constructor viewport polish

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles/tokens.css";
 import "./styles/atoms.css";
 import "./styles/constructor-layout-overrides.css";
+import "./styles/constructor-viewport-polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
