6ba650938a7f3b6b73c4890dede884b6990eed6f
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-13 17:41:34 +0300

    add constructor new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import ConstructorPage from './pages/ConstructorPageStable'
+import ConstructorPageNew from './pages/ConstructorPageNew'
 import AuthPage from './pages/AuthPage'
 import AccountPage from './pages/AccountPage'
 import AccountOrderPage from './pages/AccountOrderPage'
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/constructor" element={<ConstructorPage />} />
+        <Route path="/constructor-new" element={<ConstructorPageNew />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/account/order" element={<AccountOrderPage />} />
