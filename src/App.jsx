7e64027acb400c8d49fbd8473a33e89fabc0f316
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-13 17:06:06 +0300

    switch to stable constructor page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing        from './pages/Landing'
-import ConstructorPage from './pages/ConstructorPage'
-import AuthPage       from './pages/AuthPage'
-import AccountPage    from './pages/AccountPage'
+import Landing from './pages/Landing'
+import ConstructorPage from './pages/ConstructorPageStable'
+import AuthPage from './pages/AuthPage'
+import AccountPage from './pages/AccountPage'
 import AccountOrderPage from './pages/AccountOrderPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"                 element={<Landing />} />
-        <Route path="/constructor"      element={<ConstructorPage />} />
-        <Route path="/auth"             element={<AuthPage />} />
-        <Route path="/account"          element={<AccountPage />} />
-        <Route path="/account/order"    element={<AccountOrderPage />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/constructor" element={<ConstructorPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/account/order" element={<AccountOrderPage />} />
       </Routes>
     </BrowserRouter>
   )
