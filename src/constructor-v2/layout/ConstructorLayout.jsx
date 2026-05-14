ec0c1a95b5f7ae3c9520149ea22f4b3f2fc3d50b
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-14 23:31:20 +0300

    Mount visible SizePanel in Constructor V2

--- src/constructor-v2/layout/ConstructorLayout.jsx
+++ src/constructor-v2/layout/ConstructorLayout.jsx
@@ -2,6 +2,7 @@ import ConstructorActions from "../components/ConstructorActions";
 import ConstructorProgress from "../components/ConstructorProgress";
 import ConstructorSummary from "../components/ConstructorSummary";
 import ConstructorViewer from "../components/ConstructorViewer";
+import SizePanel from "../components/SizePanel";
 import "../styles/constructor-v2.css";
 
 export default function ConstructorLayout() {
@@ -12,12 +13,7 @@ export default function ConstructorLayout() {
       <ConstructorProgress />
 
       <div className="rv2-grid">
-        <aside className="rv2-sidebar">
-          <div className="rv2-card">
-            <span className="rv2-card-index">1</span>
-            <h3>Размеры и секции</h3>
-          </div>
-        </aside>
+        <SizePanel />
 
         <ConstructorViewer />
 
