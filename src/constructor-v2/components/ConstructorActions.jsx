ddafc03204fa7bca535272f720627848191f83cd
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-14 23:25:26 +0300

    Add Constructor V2 actions component

---
+++ src/constructor-v2/components/ConstructorActions.jsx
@@ -0,0 +1,10 @@
+export default function ConstructorActions() {
+  return (
+    <div className="rv2-actions rv2-actions-top">
+      <button type="button">Загрузить</button>
+      <button type="button">Очистить</button>
+      <button type="button">Сохранить</button>
+      <button type="button" className="is-dark">В корзину</button>
+    </div>
+  );
+}
