98edbd1c70e207a2e356f270ff1268be86d7162c
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-15 10:02:03 +0300

    Connect viewer premium motion layers

--- src/constructor-v2/layout/ConstructorLayout.jsx
+++ src/constructor-v2/layout/ConstructorLayout.jsx
@@ -9,6 +9,9 @@ import MobileConstructorBar from "../components/MobileConstructorBar";
 import SizePanel from "../components/SizePanel";
 import { useCabinetStore } from "../../store/cabinetStore";
 import "../styles/constructor-v2.css";
+import "../styles/constructor-v2-camera-parallax.css";
+import "../styles/constructor-v2-material-light.css";
+import "../styles/constructor-v2-idle-motion.css";
 
 function getItemCount(section, type) {
   return section?.items?.find((item) => item.type === type)?.count || 0;
