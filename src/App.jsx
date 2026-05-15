e25f9588706c8711b9c8e8b8ff5508fedb8c38a0
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-15 10:43:11 +0300

    Remove constructor routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
-import ConstructorPageNew from './pages/ConstructorPageNew'
 import AuthPage from './pages/AuthPage'
 import AccountPage from './pages/AccountPage'
 import AccountOrderPage from './pages/AccountOrderPage'
@@ -10,8 +9,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/constructor" element={<ConstructorPageNew />} />
-        <Route path="/constructor-new" element={<ConstructorPageNew />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/account/order" element={<AccountOrderPage />} />
