7dc56835d8e33e0129f7549dabd8083c5db54327
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-15 12:08:19 +0300

    Connect constructor page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Landing from './pages/Landing'
 import AuthPage from './pages/AuthPage'
 import AccountPage from './pages/AccountPage'
 import AccountOrderPage from './pages/AccountOrderPage'
+import ConstructorPage from './pages/ConstructorPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/constructor" element={<ConstructorPage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/account/order" element={<AccountOrderPage />} />
