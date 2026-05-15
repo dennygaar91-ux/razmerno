6adc6c39b56858d5370fd37d8328e0378867d11c
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-15 12:49:16 +0300

    Remove left rail from constructor page

--- src/pages/ConstructorPage.jsx
+++ src/pages/ConstructorPage.jsx
@@ -1,19 +1,18 @@
 import Header from '../components/Header/Header'
-import ConstructorRail from '../components/constructor/ConstructorRail'
 import ConstructorConfig from '../components/constructor/ConstructorConfig'
 import ConstructorViewer from '../components/constructor/ConstructorViewer'
 import ConstructorSummary from '../components/constructor/ConstructorSummary'
 import ConstructorBenefits from '../components/constructor/ConstructorBenefits'
 import './ConstructorPage.css'
 import './ConstructorPageHeader.css'
+import './ConstructorPageNoRail.css'
 
 export default function ConstructorPage() {
   return (
     <>
       <Header />
       <main className="rp-ctor-page">
-        <section className="rp-ctor-shell" aria-label="Конструктор шкафа">
-          <ConstructorRail />
+        <section className="rp-ctor-shell rp-ctor-shell--no-rail" aria-label="Конструктор шкафа">
           <ConstructorConfig />
           <ConstructorViewer />
           <ConstructorSummary />
