7156d6d9c35c0aea3225ed1bdff0e5d1bf59ef20
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-15 22:10:09 +0300

    Polish viewer scene overlays

--- src/components/constructor/viewer/ViewerScene.jsx
+++ src/components/constructor/viewer/ViewerScene.jsx
@@ -1,14 +1,21 @@
 import WardrobeMockup from '../WardrobeMockup'
 
 export default function ViewerScene({ sceneProps, renderCanvas }) {
-  const { dimensions } = sceneProps
+  const { dimensions, activeSection } = sceneProps
 
   return (
-    <div className="rp-ref-scene" data-viewer-scene="wardrobe">
-      <span className="rp-ctor-size rp-ctor-size--h">{dimensions.height} мм</span>
+    <div className="rp-ref-scene rp-ref-scene--polished" data-viewer-scene="wardrobe">
+      <div className="rp-ref-scene__badge">
+        <span>3D preview</span>
+        <b>Секция {activeSection}</b>
+      </div>
+
+      <span className="rp-ctor-size rp-ctor-size--h"><em>Высота</em>{dimensions.height} мм</span>
       {renderCanvas ? renderCanvas(sceneProps) : <WardrobeMockup project={sceneProps} />}
-      <span className="rp-ctor-size rp-ctor-size--w">{dimensions.width} мм</span>
-      <span className="rp-ctor-size rp-ctor-size--d">{dimensions.depth} мм</span>
+      <span className="rp-ctor-size rp-ctor-size--w"><em>Ширина</em>{dimensions.width} мм</span>
+      <span className="rp-ctor-size rp-ctor-size--d"><em>Глубина</em>{dimensions.depth} мм</span>
+
+      <div className="rp-ref-scene__hint">Выберите секцию на модели или в карте ниже</div>
     </div>
   )
 }
