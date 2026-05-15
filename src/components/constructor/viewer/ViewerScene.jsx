84590b3642ab901c759f749b16950fb64588e716
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-16 00:36:36 +0300

    Use canvas slot in viewer scene

--- src/components/constructor/viewer/ViewerScene.jsx
+++ src/components/constructor/viewer/ViewerScene.jsx
@@ -1,17 +1,23 @@
-import WardrobeMockup from '../WardrobeMockup'
+import CanvasSlot from './CanvasSlot'
 
 export default function ViewerScene({ sceneProps, renderCanvas }) {
-  const { dimensions, activeSection } = sceneProps
+  const { dimensions, activeSection, meta } = sceneProps
 
   return (
-    <div className="rp-ref-scene rp-ref-scene--polished" data-viewer-scene="wardrobe">
+    <div className="rp-ref-scene rp-ref-scene--polished" data-viewer-scene="wardrobe" data-renderer={renderCanvas ? 'three' : 'css'}>
       <div className="rp-ref-scene__badge">
-        <span>3D preview</span>
+        <span>{renderCanvas ? 'Three.js preview' : '3D preview'}</span>
         <b>Секция {activeSection}</b>
       </div>
 
+      <div className="rp-ref-scene__meta">
+        <span>{meta?.sectionWidth} мм / секц.</span>
+        <span>{meta?.materialTone}</span>
+        <span>{meta?.fillingElements} элем.</span>
+      </div>
+
       <span className="rp-ctor-size rp-ctor-size--h"><em>Высота</em>{dimensions.height} мм</span>
-      {renderCanvas ? renderCanvas(sceneProps) : <WardrobeMockup project={sceneProps} />}
+      <CanvasSlot sceneProps={sceneProps} renderCanvas={renderCanvas} />
       <span className="rp-ctor-size rp-ctor-size--w"><em>Ширина</em>{dimensions.width} мм</span>
       <span className="rp-ctor-size rp-ctor-size--d"><em>Глубина</em>{dimensions.depth} мм</span>
 
