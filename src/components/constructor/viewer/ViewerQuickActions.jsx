5f7f615f16a4b31f76406727345a010f8a471f74
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-15 22:09:15 +0300

    Polish viewer quick actions UX

--- src/components/constructor/viewer/ViewerQuickActions.jsx
+++ src/components/constructor/viewer/ViewerQuickActions.jsx
@@ -1,12 +1,29 @@
 import Icon from '../../../icons/Icon'
 
+const actions = [
+  ['shelves', 'Полка', 'Добавить горизонтальную полку'],
+  ['drawers', 'Ящик', 'Добавить выдвижной ящик'],
+]
+
 export default function ViewerQuickActions({ activeSection, railDisabled, onSectionPartChange, onRailToggle, onClearSection }) {
   return (
-    <div className="rp-ref-quick-actions">
-      <button type="button" onClick={() => onSectionPartChange('shelves', 1)}><Icon name="plus" size={16} />Полка</button>
-      <button type="button" onClick={() => onSectionPartChange('drawers', 1)}><Icon name="plus" size={16} />Ящик</button>
-      <button className={activeSection.rail ? 'is-active' : ''} type="button" disabled={railDisabled} onClick={onRailToggle}><span className="rp-ref-rail-icon" />Штанга</button>
-      <button type="button" onClick={onClearSection}><Icon name="x" size={15} />Очистить</button>
+    <div className="rp-ref-quick-actions rp-ref-quick-actions--polished" aria-label="Быстрые действия с активной секцией">
+      {actions.map(([type, title, text]) => (
+        <button type="button" key={type} onClick={() => onSectionPartChange(type, 1)}>
+          <span className="rp-ref-quick-actions__icon"><Icon name="plus" size={15} /></span>
+          <span>{title}<small>{text}</small></span>
+        </button>
+      ))}
+
+      <button className={activeSection.rail ? 'is-active' : ''} type="button" disabled={railDisabled} onClick={onRailToggle}>
+        <span className="rp-ref-quick-actions__icon"><i className="rp-ref-rail-icon" /></span>
+        <span>{activeSection.rail ? 'Штанга есть' : 'Штанга'}<small>{railDisabled ? 'Нужна глубина 520 мм' : 'Для одежды на плечиках'}</small></span>
+      </button>
+
+      <button className="is-danger-soft" type="button" onClick={onClearSection}>
+        <span className="rp-ref-quick-actions__icon"><Icon name="x" size={14} /></span>
+        <span>Очистить<small>Сбросить секцию</small></span>
+      </button>
     </div>
   )
 }
