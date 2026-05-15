a6b4e107a9396a33d640377005f08155ec68686b
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-15 12:26:37 +0300

    Add wardrobe mockup component

---
+++ src/components/constructor/WardrobeMockup.jsx
@@ -0,0 +1,22 @@
+export default function WardrobeMockup() {
+  return (
+    <div className="rp-ctor-wardrobe" aria-hidden="true">
+      <div className="rp-ctor-top" />
+      <div className="rp-ctor-col rp-ctor-col--shelves">
+        <span />
+        <span />
+        <span />
+        <b />
+        <b />
+      </div>
+      <div className="rp-ctor-col">
+        <em />
+        <strong />
+      </div>
+      <div className="rp-ctor-col">
+        <em />
+        <strong />
+      </div>
+    </div>
+  )
+}
