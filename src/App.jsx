2a2497e9910c70233a3b54b509fa9e77fa8cc3e3
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-17 23:00:45 +0300

    Render landing header on constructor routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@ import AuthPage from './pages/AuthPage'
 import AccountPage from './pages/AccountPage'
 import AccountOrderPage from './pages/AccountOrderPage'
 import ConstructorPage from './pages/ConstructorPage'
+import Header from './components/Header/Header'
+
+function ConstructorRoute() {
+  return (
+    <>
+      <Header />
+      <ConstructorPage />
+    </>
+  )
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/constructor" element={<ConstructorPage />} />
-        <Route path="/constructor/*" element={<ConstructorPage />} />
+        <Route path="/constructor" element={<ConstructorRoute />} />
+        <Route path="/constructor/*" element={<ConstructorRoute />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/account/order" element={<AccountOrderPage />} />
