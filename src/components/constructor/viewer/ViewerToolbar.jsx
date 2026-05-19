63be9571a2110b308654552bf28ecbe5cc7dddbd
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-19 19:27:59 +0300

    Make viewer toolbar control view mode

--- src/components/constructor/viewer/ViewerToolbar.jsx
+++ src/components/constructor/viewer/ViewerToolbar.jsx
@@ -1,12 +1,26 @@
 import Icon from '../../../icons/Icon'
 
-export default function ViewerToolbar() {
+export default function ViewerToolbar({ viewMode = '3d', onViewModeChange }) {
   return (
     <div className="rp-ref-viewer-toolbar">
-      <div className="rp-ref-view-mode">
+      <div className="rp-ref-view-mode" role="group" aria-label="Режим предпросмотра шкафа">
         <span>Вид</span>
-        <button className="is-active" type="button">3D</button>
-        <button type="button">2D</button>
+        <button
+          className={viewMode === '3d' ? 'is-active' : ''}
+          type="button"
+          aria-pressed={viewMode === '3d'}
+          onClick={() => onViewModeChange?.('3d')}
+        >
+          3D
+        </button>
+        <button
+          className={viewMode === '2d' ? 'is-active' : ''}
+          type="button"
+          aria-pressed={viewMode === '2d'}
+          onClick={() => onViewModeChange?.('2d')}
+        >
+          2D
+        </button>
       </div>
 
       <div className="rp-ref-scale">
