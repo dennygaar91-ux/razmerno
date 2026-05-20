dae1421ae611834bc21a7ffc16718f9456e154d0
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-20 20:12:35 +0300

    Import safe UIKit bridge

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tokens.css";
 import "./styles/atoms.css";
+import "./styles/uikit-bridge.css";
 import "./pages/ConstructorDesignTokens.css";
 import "./styles/constructor-layout-overrides.css";
 import "./styles/constructor-viewport-polish.css";
