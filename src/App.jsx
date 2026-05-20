567a7dcf92fd80c856f7816bfa51b5dd619be68e
dennygaar91-ux <dennygaar91@gmail.com>
2026-05-20 18:53:34 +0300

    Use UIKit account dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Landing from './pages/LandingUIKit'
 import AuthPage from './pages/AuthPage'
-import AccountPage from './pages/AccountPage'
+import AccountPage from './pages/AccountUIKitPage'
 import AccountOrderPage from './pages/AccountOrderPage'
 import ConstructorPage from './pages/ConstructorUIKitPage'
 
